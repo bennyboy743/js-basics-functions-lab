8ad9ff744a31bc15428cfcd85ce8909ca3a81ca7
Benjamin <benjaminrowe0@gmail.com>
2021-12-14 21:28:56 +1100

    commit tree 1d84c6e841da6ef531e250491732544f01b5d01a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(address){
+    if(address >= 42){
+        address = address-42
+        return address
+    }else {
+        return address = 42-address
+    }
+    
+}
+
+
+console.log(distanceFromHqInBlocks(4))
